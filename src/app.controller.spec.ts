417b41a2750a613b83e5a4f146d6b136a3be6e79
Wizston <wizston@gmail.com>
2024-02-05 01:02:03 +0100

    Added example .env file

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './home/home.controller';
-import { AppService } from './home/home.service';
+import { HomeController } from './home/home.controller';
+import { HomeService } from './home/home.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let homeController: HomeController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [HomeController],
+      providers: [HomeService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    homeController = app.get<HomeController>(HomeController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(homeController.getHello()).toBe('Hello World!');
     });
   });
 });
